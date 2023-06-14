b6d53515d439e7767651edbf6729dcb4b7b0b24e
serbertmart <sergi.bertran@gmail.com>
2023-06-14 18:27:41 +0200

    Exercici 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  
+  const [webIsChecked, setWebIsChecked] = useState(true);
+  const [seoIsChecked, setSeoIsChecked] = useState(true);
+  const [semIsChecked, setSemIsChecked] = useState(true);
+  const [total, setTotal]= useState(0);
+
+
+  const checkWeb = () => {
+    setWebIsChecked(!webIsChecked);
+    if (webIsChecked == true) {
+      setTotal(total + 500);
+    } else {setTotal(total - 500)}
+  }
+
+  const checkSeo = () => {
+    setSeoIsChecked(!seoIsChecked);
+    if (seoIsChecked == true) {
+      setTotal(total + 300);
+    } else {setTotal(total - 300)}
+  }
+
+  const checkSem = () => {
+    setSemIsChecked(!semIsChecked);
+    if (semIsChecked == true) {
+      setTotal(total + 200);
+    } else {setTotal(total - 200)}
+  }
+
+  console.log(total);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="divprincipal">
+      <h3>Que vols fer?</h3>
+      <div>
+        <input
+          type="checkbox"
+          id="web"
+          name="web"
+          value="web"
+          onChange={checkWeb}
+        />
+        Una página web (500€)
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          id="seo"
+          name="seo"
+          value="seo"
+          onChange={checkSeo}
+        />
+        Una consultoria SEO (300€)
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          id="ads"
+          name="ads"
+          value="ads"
+          onChange={checkSem}
+        />
+        Una campaña de google ads (200€)
+      </div>
+      <div className="resultat">
+        El preu es de: {total} €
+      </div>
     </div>
   );
 }
